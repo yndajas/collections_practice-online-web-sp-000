5b658c5ee24558a09b9c5c9a34f808618af728bb
Ynda Jas <yndajas@gmail.com>
2020-07-06 14:57:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array [1]
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(strings)
+  strings.collect do |string|
+    string[2] = "$"
+    string
+  end
+end
+
+def find_a(strings)
+  strings.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(integers)
+  integers.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |item,index|
+    index != 1 ? item << "s" : item
+  end
+end
